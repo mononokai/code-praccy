1387572467206cfd53e33898c8f2a14ce3ecfd9c
david <leachda12@gmail.com>
2022-11-02 18:19:08 -0400

    added some glowy

--- glowy-boy/index.js
+++ glowy-boy/index.js
@@ -1,5 +1,16 @@
 const head = $('.head');
 
-function glowControl(m) {
+// function glowControl(m) {
+//     const headView = m.target.getBoundingClientRect();
+//     const mouseX = m.target.clientX - rect.left;
+//     const mouseY = m.target.clientY - rect.top;
 
-}
+//     target.style.setProperty('--mouse-x', `${mouseX}px`);
+//     target.style.setProperty('--mouse-y'), `${mouseY}px`
+// }
+
+$('.head').mousemove(e => {
+    e.target.style.setProperty('--mouse-x', `${e.pageX}px`);
+    e.target.style.setProperty('--mouse-y', `${e.pageY}px`);
+    console.log(e.target.style('--mouse-x'))
+})
