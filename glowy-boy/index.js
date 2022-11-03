9109d831aff9bd9f4b84d847292f4ad263eb635a
david <leachda12@gmail.com>
2022-11-03 01:11:58 -0400

    IT'S ALIVE

--- glowy-boy/index.js
+++ glowy-boy/index.js
@@ -1,16 +1,7 @@
-const head = $('.head');
-
-// function glowControl(m) {
-//     const headView = m.target.getBoundingClientRect();
-//     const mouseX = m.target.clientX - rect.left;
-//     const mouseY = m.target.clientY - rect.top;
-
-//     target.style.setProperty('--mouse-x', `${mouseX}px`);
-//     target.style.setProperty('--mouse-y'), `${mouseY}px`
-// }
-
 $('.head').mousemove(e => {
-    e.target.style.setProperty('--mouse-x', `${e.pageX}px`);
-    e.target.style.setProperty('--mouse-y', `${e.pageY}px`);
-    console.log(e.target.style('--mouse-x'))
+    e.target.style.setProperty('--xpos', `${e.pageX}px`);
+    e.target.style.setProperty('--ypos', `${e.pageY}px`);
+    $('.light').css('left', `${e.pageX}px`);
+    $('.light').css('top', `${e.pageY}px`);
 })
+
