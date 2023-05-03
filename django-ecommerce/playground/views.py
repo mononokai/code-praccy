5747eb8daf62fffaaa4388b13f071bfc02da1439
david <leachda12@icloud.com>
2023-05-03 05:49:29 -0400

    done with the short django project

--- django-ecommerce/playground/views.py
+++ django-ecommerce/playground/views.py
@@ -6,6 +6,12 @@ from django.http import HttpResponse
 # it is a request handler, in some languages it is called an action
 
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 
 def say_hello(request):
+    x = calculate()
     return render(request, 'hello.html', {'name': 'David'})
