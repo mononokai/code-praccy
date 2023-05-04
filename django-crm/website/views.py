b9a7a83e2b72b54b29b31d6713f64382226eeb54
david <leachda12@icloud.com>
2023-05-04 19:44:52 -0400

    finished up login

--- django-crm/website/views.py
+++ django-crm/website/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def index(request):
-    return render(request, 'index.html')
+    # Check for login
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('index')
+        else:
+            messages.success(request, "There was an error logging in, please try again")
+            return redirect('index')
+    else:
+        return render(request, 'index.html')
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
